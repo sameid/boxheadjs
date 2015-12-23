597e1049b6c9a70699550400a38b37d237416df9
Sameid <sameid.usmani@gmail.com>
2015-12-23 04:14:49 +0000

    skeleton

--- run.js
+++ run.js
@@ -1,1 +1,21 @@
-var i = 10;
+var init = function(){
+    
+}
+
+
+var update = function(){
+    
+}
+
+
+var paint = function(){
+    
+}
+
+var run = function(){
+    update();
+    paint();
+}
+
+init();
+setInterval(run, 60/1000);
